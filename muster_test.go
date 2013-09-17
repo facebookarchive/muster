2ee3cb527e765f2b786fc9e8dcc52966095ee428
Naitik Shah <n@daaku.org>
2013-09-17 12:51:27 -0700

    timeout test

--- muster_test.go
+++ muster_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestMaxBatch(t *testing.T) {
+	t.Parallel()
 	expected := []string{"milk", "yogurt", "butter"}
 	delivery := make(chan []string)
 	finished := make(chan struct{})
@@ -32,5 +33,36 @@ func TestMaxBatch(t *testing.T) {
 	for _, v := range expected {
 		sm.Add(v)
 	}
+	<-finished
+}
+
+func TestBatchTimeout(t *testing.T) {
+	t.Parallel()
+	expected := []string{"milk", "yogurt"}
+	delivery := make(chan []string)
+	finished := make(chan struct{})
+	go func() {
+		defer close(finished)
+		actual := <-delivery
+		if !reflect.DeepEqual(actual, expected) {
+			t.Fatal("did not find expected batch")
+		}
+	}()
+
+	sm := &ShoppingManager{
+		ShopperCapacity: 3,
+		TripTimeout:     20 * time.Millisecond,
+		PendingCapacity: 100,
+		Delivery:        delivery,
+	}
+
+	if err := sm.Start(); err != nil {
+		t.Fatal(err)
+	}
+
+	for _, v := range expected {
+		sm.Add(v)
+	}
+	time.Sleep(30 * time.Millisecond)
 	<-finished
 }
