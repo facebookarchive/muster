565ac50615b339eec5c3d2dbca984bf3a5ca7038
Naitik Shah <n@daaku.org>
2013-09-17 14:35:58 -0700

    reusable test components

--- muster_test.go
+++ muster_test.go
@@ -46,55 +46,63 @@ func (b *testBatch) Fire(notifier muster.Notifier) {
 	b.testClient.Fire(b.Items, notifier)
 }
 
-func TestMaxBatch(t *testing.T) {
-	t.Parallel()
-	expected := []string{"milk", "yogurt", "butter"}
-	finished := make(chan struct{})
-	sm := &testClient{
-		MaxBatchSize:    3,
-		BatchTimeout:    20 * time.Millisecond,
-		PendingCapacity: 100,
-		Fire: func(actual []string, notifier muster.Notifier) {
-			defer close(finished)
-			if !reflect.DeepEqual(actual, expected) {
-				t.Fatal("did not find expected batch")
-			}
-		},
-	}
-
-	if err := sm.Start(); err != nil {
+func errCall(t *testing.T, f func() error) {
+	if err := f(); err != nil {
 		t.Fatal(err)
 	}
+}
 
-	for _, v := range expected {
-		sm.Add(v)
+func expectFire(
+	t *testing.T,
+	finished chan struct{},
+	expected [][]string,
+) func(actual []string, notifier muster.Notifier) {
+
+	return func(actual []string, notifier muster.Notifier) {
+		defer close(finished)
+		for _, batch := range expected {
+			if !reflect.DeepEqual(actual, batch) {
+				t.Fatalf("expected %v\nactual %v", batch, actual)
+			}
+		}
 	}
+}
+
+func addExpected(c *testClient, expected [][]string) {
+	for _, b := range expected {
+		for _, v := range b {
+			c.Add(v)
+		}
+	}
+}
+
+func TestMaxBatch(t *testing.T) {
+	t.Parallel()
+	expected := [][]string{{"milk", "yogurt", "butter"}}
+	finished := make(chan struct{})
+	c := &testClient{
+		MaxBatchSize:    len(expected[0][0]),
+		BatchTimeout:    20 * time.Millisecond,
+		Fire:            expectFire(t, finished, expected),
+		PendingCapacity: 100,
+	}
+	errCall(t, c.Start)
+	addExpected(c, expected)
 	<-finished
 }
 
 func TestBatchTimeout(t *testing.T) {
 	t.Parallel()
-	expected := []string{"milk", "yogurt"}
+	expected := [][]string{{"milk", "yogurt"}}
 	finished := make(chan struct{})
-	sm := &testClient{
+	c := &testClient{
 		MaxBatchSize:    3,
 		BatchTimeout:    20 * time.Millisecond,
+		Fire:            expectFire(t, finished, expected),
 		PendingCapacity: 100,
-		Fire: func(actual []string, notifier muster.Notifier) {
-			defer close(finished)
-			if !reflect.DeepEqual(actual, expected) {
-				t.Fatal("did not find expected batch")
-			}
-		},
 	}
-
-	if err := sm.Start(); err != nil {
-		t.Fatal(err)
-	}
-
-	for _, v := range expected {
-		sm.Add(v)
-	}
+	errCall(t, c.Start)
+	addExpected(c, expected)
 	time.Sleep(30 * time.Millisecond)
 	<-finished
 }
