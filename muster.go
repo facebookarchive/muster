f045bb97907eb64cb1bf5b05e3851e105ca9d566
Naitik Shah <n@daaku.org>
2015-01-12 17:01:39 -0800

    reuse the timer instead of allocating a new one

--- muster.go
+++ muster.go
@@ -117,13 +117,16 @@ func (c *Client) worker() {
 	defer c.workGroup.Done()
 	var batch = c.BatchMaker()
 	var count uint
+	var batchTimer *time.Timer
 	var batchTimeout <-chan time.Time
 	send := func() {
 		c.workGroup.Add(1)
 		go batch.Fire(c.workGroup)
 		batch = c.BatchMaker()
 		count = 0
-		batchTimeout = nil
+		if batchTimer != nil {
+			batchTimer.Stop()
+		}
 	}
 	recv := func(item interface{}, open bool) bool {
 		if !open {
@@ -136,8 +139,13 @@ func (c *Client) worker() {
 		count++
 		if c.MaxBatchSize != 0 && count >= c.MaxBatchSize {
 			send()
-		} else if int64(c.BatchTimeout) != 0 && batchTimeout == nil {
-			batchTimeout = time.After(c.BatchTimeout)
+		} else if int64(c.BatchTimeout) != 0 && count == 1 {
+			if batchTimer == nil {
+				batchTimer = time.NewTimer(c.BatchTimeout)
+				batchTimeout = batchTimer.C
+			} else {
+				batchTimer.Reset(c.BatchTimeout)
+			}
 		}
 		return false
 	}
